bb79ba6ec5742fb6375e4f102d089341cbe4afd3
sourland <jem2010@windowslive.com>
2023-04-25 19:17:16 +0300

    code reformat

--- object_detection/hand_landmarks.py
+++ object_detection/hand_landmarks.py
@@ -5,7 +5,8 @@ import cv2
 import numpy as np
 from draw_landmarks_on_image import draw_landmarks_on_image
 
-def resize_image(img: np.ndarray, new_size:int) -> np.ndarray:
+
+def resize_image(img: np.ndarray, new_size: int) -> np.ndarray:
     """
     Resizes the image by resizing the smaller axis to the desired size in order to maintain aspect ratio.
 
@@ -14,6 +15,8 @@ def resize_image(img: np.ndarray, new_size:int) -> np.ndarray:
         new_size: the new size of the smaller axis
 
     Returns: Resized image
+
+    Raises: None
     """
     scale_percent = new_size / min((img.shape[0], img.shape[1]))
     width = int(img.shape[1] * scale_percent)
@@ -23,19 +26,58 @@ def resize_image(img: np.ndarray, new_size:int) -> np.ndarray:
     return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 
-base_options = python.BaseOptions(model_asset_path='../hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=1, min_hand_detection_confidence=0.13)
-# detector
-detector = vision.HandLandmarker.create_from_options(options)
-img = cv2.imread('../hand.jpg', cv2.IMREAD_UNCHANGED)
+def load_hand_landmarker(path: str) -> vision.HandLandmarker:
+    """
+    Loads a hand landmark detection model from the specified path and returns a HandLandmarker object.
+
+    Args:
+        path (str): The path to the model asset file.
+
+    Returns:
+        vision.HandLandmarker: A HandLandmarker object for detecting landmarks on hands in images.
+
+    Raises:
+        None
+    """
+    # Define the base options for loading the model
+    base_options = python.BaseOptions(model_asset_path=path)
+
+    # Set options for the HandLandmarker object
+    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=1, min_hand_detection_confidence=0.13)
+
+    # Create and return the HandLandmarker object
+    return vision.HandLandmarker.create_from_options(options)
+
+
+def locate_hand_landmarks(image: np.ndarray, detector: vision.HandLandmarker) -> vision.HandLandmarkerResult:
+    """
+    Detects hand landmarks in the input image using the specified HandLandmarker object.
+
+    Args:
+        image (np.ndarray): The input image to detect hand landmarks on.
+        detector (vision.HandLandmarker): A HandLandmarker object for detecting landmarks on hands in images.
+
+    Returns:
+        vision.HandLandmarkerResult: The result of the hand landmark detection, which includes the detected landmarks and their confidence scores.
+
+    Raises:
+        None
+    """
+    # Convert the input image to a mediapipe image
+    mediapipe_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+
+    # Use the HandLandmarker object to detect hand landmarks in the mediapipe image
+    return detector.detect(mediapipe_image)
+
+
+
+img = cv2.imread('../hand3.jpg', cv2.IMREAD_UNCHANGED)
 resized_img = resize_image(img, 300)
-resized_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=resized_img)
-detection_result = detector.detect(resized_img)
-annotated_image = draw_landmarks_on_image(resized_img.numpy_view(), detection_result)
+detector = load_hand_landmarker('../hand_landmarker.task')
+landmarks = locate_hand_landmarks(resized_img, detector)
+annotated_image = draw_landmarks_on_image(
+    mp.Image(image_format=mp.ImageFormat.SRGB, data=resized_img).numpy_view(),
+    landmarks
+)
 cv2.imshow('image', annotated_image)
 cv2.waitKey(0)
-
-
-
-
-
