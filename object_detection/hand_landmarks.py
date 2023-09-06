17159652300ca97ccfee39fd280584f70ccbd0bb
sourland <jem2010@windowslive.com>
2023-09-06 19:04:16 +0300

    add circle drawing

--- object_detection/hand_landmarks.py
+++ object_detection/hand_landmarks.py
@@ -1,33 +1,32 @@
+import cv2
 import math
 from utils import *
-import cv2
-from pixel_finder import find_bounding_box, crop_image
 from landmarks_constants import *
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from pixel_finder import find_bounding_box, crop_image
+
 # from joint_thickness import get_finger_vectors
 
 BG_COLOR = (0, 0, 0)  # gray
 MASK_COLOR = (255, 255, 255)  # white
 HAND_PATH = '../hands/hand1.jpg'
-
-img = cv2.imread(HAND_PATH, cv2.IMREAD_UNCHANGED)
-detector = load_hand_landmarker('../hand_landmarker.task')
-landmarks = locate_hand_landmarks(HAND_PATH, detector)
-which_hand = landmarks.handedness[0][0].category_name
-annotated_image = draw_landmarks_on_image(
-    mp.Image(image_format=mp.ImageFormat.SRGB, data=img).numpy_view(),
-    landmarks
-)
-cv2.imwrite('../landmarksHand.jpg', annotated_image)
-
-
-# Height and width that will be used by the model
 DESIRED_HEIGHT = 480
 DESIRED_WIDTH = 480
 
 
-# Performs resizing and showing the image
+def load_and_annotate_image(hand_path, detector_path):
+    img = cv2.imread(hand_path, cv2.IMREAD_UNCHANGED)
+    detector = load_hand_landmarker(detector_path)
+    landmarks = locate_hand_landmarks(hand_path, detector)
+    annotated_image = draw_landmarks_on_image(
+        mp.Image(image_format=mp.ImageFormat.SRGB, data=img).numpy_view(),
+        landmarks
+    )
+    cv2.imwrite('../landmarksHand.jpg', annotated_image)
+    return img, landmarks
+
+
 def resize_and_show(image):
     h, w = image.shape[:2]
     if h < w:
@@ -35,27 +34,20 @@ def resize_and_show(image):
     else:
         img = cv2.resize(image, (math.floor(w / (h / DESIRED_HEIGHT)), DESIRED_HEIGHT))
     cv2.imwrite('../seg_mask.jpg', img)
+    return img
 
 
-# Create the options that will be used for ImageSegmenter
-base_options = python.BaseOptions(model_asset_path='../selfie_multiclass_256x256.tflite')
-options = vision.ImageSegmenterOptions(
-    base_options=base_options  # Locale to use for segment display names (assuming English US here)
-)
-# Create the image segmenter
-with vision.ImageSegmenter.create_from_options(options) as segmenter:
-    # Create the MediaPipe image file that will be segmented
-    image = mp.Image.create_from_file(HAND_PATH)
+def segment_image(image, model_path):
+    base_options = python.BaseOptions(model_asset_path=model_path)
+    options = vision.ImageSegmenterOptions(base_options=base_options)
+    with vision.ImageSegmenter.create_from_options(options) as segmenter:
+        segmentation_result = segmenter.segment(image)
+        segmentation_mask = segmentation_result[0].numpy_view()
+    return segmentation_mask
 
-    # Retrieve the masks for the segmented image
-    segmentation_result = segmenter.segment(image)
-    segmentation_mask = segmentation_result[0].numpy_view()
 
-    # Thresholding the mask
+def process_mask(segmentation_mask, image_data):
     thresholded_mask = np.where(segmentation_mask > 0.1, 1, 0)
-
-    # Generate solid color images for showing the output segmentation mask
-    image_data = image.numpy_view()
     fg_image = np.zeros(image_data.shape, dtype=np.uint8)
     fg_image[:] = MASK_COLOR
     bg_image = np.zeros(image_data.shape, dtype=np.uint8)
@@ -63,31 +55,31 @@ with vision.ImageSegmenter.create_from_options(options) as segmenter:
 
     condition = np.stack((thresholded_mask,) * 3, axis=-1)
     output_image = np.where(condition, fg_image, bg_image)
-
-    annotated_image = draw_landmarks_on_image(
-        mp.Image(image_format=mp.ImageFormat.SRGB, data=output_image).numpy_view(),
-        landmarks
-    )
-    cv2.imwrite('../landmarksHandMask.jpg', annotated_image)
-
-    resize_and_show(output_image)
+    return output_image, thresholded_mask
 
 
-landmarks = landmarks.hand_landmarks[0]
-"""
-Mediapipe landmarks are normalized in [0,1]. Use width and height to extract the landmark position in pixel coordinates
-"""
-
-"""
-From the landmark 
-"""
-
-for area in areas_of_interest:
-    top_left, bottom_right = find_bounding_box(thresholded_mask, landmarks, area, which_hand)
-    extracted_image = crop_image(img, top_left, bottom_right)
-    extracted_image = resize_image(extracted_image, 244)
-    cv2.imwrite(f"../results/area{area}.jpg", extracted_image)
-    # cv2.rectangle(img, top_left, bottom_right, (255, 0, 0), thickness=3)
-    cv2.imwrite("../boxes.jpg", img)
+def extract_regions(img, thresholded_mask, landmarks):
+    landmarks = landmarks.hand_landmarks[0]
+    for area in areas_of_interest:
+        top_left, bottom_right = find_bounding_box(thresholded_mask, landmarks, area, which_hand)
+        extracted_image = crop_image(img, top_left, bottom_right)
+        extracted_image = resize_image(extracted_image, 244)
+        cv2.imwrite(f"../results/area{area}.jpg", extracted_image)
+        cv2.imwrite("../boxes.jpg", img)
 
 
+image, landmarks = load_and_annotate_image(HAND_PATH, '../hand_landmarker.task')
+which_hand = landmarks.handedness[0][0].category_name
+
+mp_image = mp.Image.create_from_file(HAND_PATH)
+segmentation_mask = segment_image(mp_image, '../selfie_multiclass_256x256.tflite')
+output_image, thresholded_mask = process_mask(segmentation_mask, mp_image.numpy_view())
+
+annotated_image = draw_landmarks_on_image(
+    mp.Image(image_format=mp.ImageFormat.SRGB, data=output_image).numpy_view(),
+    landmarks
+)
+cv2.imwrite('../landmarksHandMask.jpg', annotated_image)
+
+resize_and_show(output_image)
+extract_regions(image, thresholded_mask, landmarks)
--- object_detection/joint_thickness.py
+++ object_detection/joint_thickness.py
@@ -3,86 +3,133 @@ import numpy as np
 from hand_landmarks import landmarks
 from pixel_finder import landmark_to_pixels
 from landmarks_constants import *
+from utils import draw_landmarks_on_image
 
-output_image = cv2.imread('../seg_mask.jpg')
-
-landmarks_pixel = [landmark_to_pixels(cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY), landmarks, idx) for idx, landmark
-                   in enumerate(landmarks)]
-landmark_depth = [landmark.z for landmark in landmarks]
-# Load image and find contour
-image = output_image
+landmarks_components = landmarks.hand_landmarks[0]
 
 
-def get_joint_thickness_euclidian(image, landmark_point):
-    """
-    Given an image and a landmark point, this function computes the thickness of the finger at that point.
-    Args:
-        image: The image containing the hand
-        landmark_point: The landmark point of the finger
-    Returns:
-        thickness: The thickness of the finger at the given point
-    """
-    height, width, _ = image.shape
+def landmarks_to_pixel_coordinates(image_path):
+    output_image = cv2.imread(image_path)
+    landmarks_pixel = [landmark_to_pixels(cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY), landmarks_components, idx)
+                       for idx, landmark in enumerate(landmarks_components)]
+    return landmarks_pixel, output_image
+
+
+def extract_contour(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresholded = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return max(contours, key=cv2.contourArea)
 
-    # Assuming the largest contour corresponds to the desired object
-    contour = max(contours, key=cv2.contourArea)
 
-    # Given point
+def plot_contour(image, contour):
+    # Copy the image to avoid modifying the original
+    image_copy = image.copy()
+    # Draw the contour on the copied image
+    cv2.drawContours(image_copy, [contour], 0, (0, 255, 0), 2)  # Drawing the contour in green color
+    # Display the image with contour
+    cv2.imshow('Image with Contour', image_copy)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def get_joint_thickness_euclidian(image_contour, landmark_point):
     point = np.array(landmark_point)
+    left_contour = image_contour[image_contour[:, 0, 0] < point[0]]
+    right_contour = image_contour[image_contour[:, 0, 0] > point[0]]
 
-    # Split the contour points into left and right based on the x-coordinate
-    left_contour = contour[contour[:, 0, 0] < point[0]]
-    right_contour = contour[contour[:, 0, 0] > point[0]]
-
-    # Compute distances from the given point to each side of the contour
     distances_left = np.sqrt(np.sum((left_contour - point) ** 2, axis=2))
     distances_right = np.sqrt(np.sum((right_contour - point) ** 2, axis=2))
 
-    # Get the closest point from the left and the right
     closest_left = left_contour[np.argmin(distances_left)]
     closest_right = right_contour[np.argmin(distances_right)]
-
-    # Compute thickness
     thickness = np.linalg.norm((closest_left - closest_right))
-
-    return thickness
-
-
-landmarks_to_process = {
-    "INDEX_FINGER_PIP": 6,
-    "INDEX_FINGER_DIP": 7,
-    "MIDDLE_FINGER_PIP": 10,
-    "MIDDLE_FINGER_DIP": 11,
-    "RING_FINGER_PIP": 14,
-    "RING_FINGER_DIP": 15,
-    "PINKY_PIP": 18,
-    "PINKY_DIP": 19
-}
-
-results = {}
+    return closest_left, closest_right, thickness
 
 
 def get_mean_pip_dip_distance(landmarks):
     landmarks = np.array(landmarks)
-    index_pip_dip_distance = np.linalg.norm(landmarks[INDEX_FINGER_PIP] - landmarks[INDEX_FINGER_DIP])
-    middle_finger_pip_dip_distance = np.linalg.norm(landmarks[MIDDLE_FINGER_PIP] - landmarks[MIDDLE_FINGER_DIP])
-    ring_pip_dip_distance = np.linalg.norm(landmarks[RING_FINGER_PIP] - landmarks[RING_FINGER_DIP])
-    pinky_pip_dip_distance = np.linalg.norm(landmarks[PINKY_PIP] - landmarks[PINKY_DIP])
-    return np.mean([index_pip_dip_distance, middle_finger_pip_dip_distance, ring_pip_dip_distance, pinky_pip_dip_distance])
+    distances = [
+        np.linalg.norm(landmarks[INDEX_FINGER_PIP] - landmarks[INDEX_FINGER_DIP]),
+        np.linalg.norm(landmarks[MIDDLE_FINGER_PIP] - landmarks[MIDDLE_FINGER_DIP]),
+        np.linalg.norm(landmarks[RING_FINGER_PIP] - landmarks[RING_FINGER_DIP]),
+        np.linalg.norm(landmarks[PINKY_PIP] - landmarks[PINKY_DIP])
+    ]
+    return np.mean(distances)
 
 
-for landmark_name, landmark_index in landmarks_to_process.items():
-    landmark_point = landmarks_pixel[landmark_index]
-    thickness = get_joint_thickness_euclidian(output_image, landmark_point)
-
-    results[landmark_name] = {
-        "thickness": thickness
+def process_landmarks(image_contour, landmarks_pixel):
+    landmarks_to_process = {
+        "INDEX_FINGER_PIP": 6,
+        "INDEX_FINGER_DIP": 7,
+        "MIDDLE_FINGER_PIP": 10,
+        "MIDDLE_FINGER_DIP": 11,
+        "RING_FINGER_PIP": 14,
+        "RING_FINGER_DIP": 15,
+        "PINKY_PIP": 18,
+        "PINKY_DIP": 19
     }
-mean_distance = get_mean_pip_dip_distance(landmarks_pixel)
-for key in results.keys():
-    print(f"{key} EFFECTIVE WIDTH: {results[key]['thickness'] / mean_distance}")
+    results = {}
+    for landmark_name, landmark_index in landmarks_to_process.items():
+        landmark_point = landmarks_pixel[landmark_index]
+        closest_left, closest_right, thickness = get_joint_thickness_euclidian(image_contour, landmark_point)
+        results[landmark_name] = {
+            "id": landmark_name,
+            "thickness": thickness,
+            "closest_left": closest_left,
+            "closest_right": closest_right
+        }
+    return results
 
-print()
+
+def draw_closest_points(image, landmark_point, contour):
+    point = np.array(landmark_point)
+
+    left_contour = contour[contour[:, 0, 0] < point[0]]
+    right_contour = contour[contour[:, 0, 0] > point[0]]
+
+    distances_left = np.sqrt(np.sum((left_contour - point) ** 2, axis=2))
+    distances_right = np.sqrt(np.sum((right_contour - point) ** 2, axis=2))
+
+    closest_left = left_contour[np.argmin(distances_left)]
+    closest_right = right_contour[np.argmin(distances_right)]
+
+    # cv2.circle(image, tuple(point), 3, (0, 255, 0), -1)  # Green circle for landmark
+    cv2.circle(image, tuple(closest_left[0]), 3, (0, 0, 255), -1)  # Red circle for left point
+    cv2.circle(image, tuple(closest_right[0]), 3, (0, 0, 255), -1)  # Red circle for right point
+
+    cv2.line(image, tuple(point), tuple(closest_left[0]), (0, 0, 255), 1)  # Line to left point
+    cv2.line(image, tuple(point), tuple(closest_right[0]), (0, 0, 255), 1)  # Line to right point
+
+    return image
+
+
+if __name__ == "__main__":
+    landmarks_to_process = {
+        "INDEX_FINGER_PIP": 6,
+        "INDEX_FINGER_DIP": 7,
+        "MIDDLE_FINGER_PIP": 10,
+        "MIDDLE_FINGER_DIP": 11,
+        "RING_FINGER_PIP": 14,
+        "RING_FINGER_DIP": 15,
+        "PINKY_PIP": 18,
+        "PINKY_DIP": 19
+    }
+    landmarks_pixel, output_image = landmarks_to_pixel_coordinates('../seg_mask.jpg')
+    image_contour = extract_contour(output_image)
+    plot_contour(output_image, image_contour)
+    results = process_landmarks(image_contour, landmarks_pixel)
+    mean_distance = get_mean_pip_dip_distance(landmarks_pixel)
+
+    for key in results.keys():
+        print(f"{key} EFFECTIVE WIDTH: {results[key]['thickness'] / mean_distance}")
+
+        # Draw closest points for each landmark on the output_image
+        output_image = draw_closest_points(output_image, landmarks_pixel[landmarks_to_process[key]], image_contour)
+
+    output_image = draw_landmarks_on_image(output_image, landmarks)
+    # Save or show the modified image with drawn points
+    cv2.imwrite('output_with_points.jpg', output_image)
+    cv2.imshow('Modified Image', output_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
