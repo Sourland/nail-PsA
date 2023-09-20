bfda14c3ceb92f945c99d0bb642cd67afa4f93cd
sourland <jem2010@windowslive.com>
2023-09-20 12:32:33 +0300

    add poetry project

--- download.py
+++ download.py
@@ -4,18 +4,21 @@ import cv2 as cv
 import json
 
 rf = Roboflow(api_key="pO2frbDKGssTk8wmtQwy")
-project = rf.workspace("knm").project("nail-disease-detection-mxoqy")
-dataset = project.version(4).download("coco")
+# project = rf.workspace("knm").project("nail-disease-detection-mxoqy")
+# dataset = project.version(4).download("coco")
+#
+#
+# f = open("dataset/Nail-Disease-Detection-4/train/_annotations.coco.json")
+# nail_disease_detection_4 = json.load(f)
+#
+#
+# project = rf.workspace("rajarata-university-of-sri-lanka").project("nail-disease-detection-system")
+# nail_disease_detection_system_2 = project.version(2).download("coco")
+#
+#
+# project = rf.workspace("tugas-akhir-kx2jl").project("nail-disease-jmapt")
+# nail_disease_detection_dataset = project.version(1).download("coco")
 
+project = rf.workspace("dataset/mini-project-xlocq").project("nailcheck")
+NailCheck_Image_dataset = project.version(1).download("folder")
 
-f = open("dataset/Nail-Disease-Detection-4/train/_annotations.coco.json")
-nail_disease_detection_4 = json.load(f)
-
-
-project = rf.workspace("rajarata-university-of-sri-lanka").project("nail-disease-detection-system")
-nail_disease_detection_system_2 = project.version(2).download("coco")
-
-
-project = rf.workspace("tugas-akhir-kx2jl").project("nail-disease-jmapt")
-nail_disease_detection_dataset = project.version(1).download("coco")
-i = 2
--- object_detection/fingers.py
+++ object_detection/fingers.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 from numpy import ndarray
 
