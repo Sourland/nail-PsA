db34d150fb13ee8dfdd04edb4d5f44593bc772a1
sourland <jem2010@windowslive.com>
2023-10-31 22:50:27 +0200

    add get contours

--- object_detection/fingers2.py
+++ object_detection/fingers2.py
@@ -6,9 +6,9 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import mediapipe as mp
 from landmarks_constants import landmarks_per_finger, joint_neighbours_right_hand, joint_neighbours_left_hand
-from object_detection.utils import load_hand_landmarker, locate_hand_landmarks
+from object_detection.pixel_finder import landmark_to_pixels
+from utils import load_hand_landmarker, locate_hand_landmarks, draw_landmarks_on_image
 import subprocess
-
 
 def get_landmarks(image_path, detector_path):
     img = cv2.imread(image_path, cv2.COLOR_RGBA2RGB)
@@ -17,107 +17,89 @@ def get_landmarks(image_path, detector_path):
     return landmarks
 
 
-def get_landmarks_pixel(landmarks, image_shape):
-    h, w = image_shape
-    landmarks_pixel = []
-    for landmark in landmarks:
-        if isinstance(landmark, tuple):
-            x = int(landmark[0] * w)
-            y = int(landmark[1] * h)
-        else:
-            x = int(landmark.x * w)
-            y = int(landmark.y * h)
-        landmarks_pixel.append((x, y))
-    return landmarks_pixel
+def load_image_and_locate_landmarks(hand_path, detector_path):
+    img = cv2.imread(hand_path, cv2.COLOR_RGBA2RGB)
+    detector = load_hand_landmarker(detector_path)
+    landmarks = locate_hand_landmarks(hand_path, detector)
+
+    return img, landmarks
+
+def get_segmentation_mask(image: np.ndarray, threshold: int = 11) -> np.ndarray:
+    """
+    Generate a binary segmentation mask based on pixel intensity.
+
+    Parameters:
+    - image: an RGB image in numpy array format.
+    - threshold: pixel values above this threshold will be set to 1, below will be set to 0.
+
+    Returns:
+    - Binary segmentation mask as numpy array.
+    """
+
+    # Check if the image has three channels
+    if len(image.shape) != 3 or image.shape[2] != 3:
+        raise ValueError("Expected an RGB image with 3 channels. Received image with shape {}.".format(image.shape))
+
+    # Convert the image to grayscale
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    # Generate the mask
+    mask = np.where(grayscale_image > threshold, 255, 0)
+
+    return mask.astype(np.uint8)
+    
+
+def extract_contour(image: np.ndarray) -> np.ndarray:
+    print(image.dtype)
+    # Threshold the grayscale image
+    # _, thresholded = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    
+    # Find contours
+    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Return the largest contour by area
+    return max(contours, key=cv2.contourArea)
 
 
-def get_finger_roi(landmark_data, finger: str, lambda_val=2 / 3):
-    # Extract indices of the landmarks of the index finger
-    finger_indices = [landmark for landmark in landmarks_per_finger[finger]]
-
-    # Use these indices to extract the landmarks from the provided data
-    landmarks = [landmark_data[i] for i in finger_indices]
-    neighbors = []
-    for index in finger_indices:
-        neighbors.append(joint_neighbours_left_hand[index])
-    neighbors = np.array(neighbors)
-
-    finger_tip = landmarks[-1]
-    left_middle_tip = ((1 - lambda_val) * finger_tip.x + lambda_val * landmark_data[neighbors[-1, 0]].x,
-                       (1 - lambda_val) * finger_tip.y + lambda_val * landmark_data[neighbors[-1, 0]].y)
-
-    right_middle_tip = ((1 - lambda_val) * finger_tip.x + lambda_val * landmark_data[neighbors[-1, 1]].x,
-                        (1 - lambda_val) * finger_tip.y + lambda_val * landmark_data[neighbors[-1, 1]].y)
-    finger_mcp = landmarks[0]
-    left_middle_mcp = ((1 - lambda_val) * finger_mcp.x + lambda_val * landmark_data[neighbors[0, 0]].x,
-                       (1 - lambda_val) * finger_mcp.y + lambda_val * landmark_data[neighbors[0, 0]].y)
-
-    right_middle_mcp = ((1 - lambda_val) * finger_mcp.x + lambda_val * landmark_data[neighbors[0, 1]].x,
-                        (1 - lambda_val) * finger_mcp.y + lambda_val * landmark_data[neighbors[0, 1]].y)
-
-    return [right_middle_tip, left_middle_tip, left_middle_mcp, right_middle_mcp]
 
 
-def get_bounding_box(points):
-    # Convert the points into a numpy array of shape Nx2
-    points_np = np.array(points, dtype=np.int32).reshape((-1, 1, 2))
-
-    # Compute the bounding box around the points
-    x, y, w, h = cv2.boundingRect(points_np)
-    return x, y, w, h
+def landmarks_to_pixel_coordinates(image, landmarks) -> list:
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return [landmark_to_pixels(gray, landmarks.hand_landmarks[0], idx) for idx in
+            range(len(landmarks.hand_landmarks[0]))]
 
 
-# if __name__ == "__main__":
-#     PATH = "../dataset/hands/swolen/hand81.jpg"
-#     image = cv2.imread(PATH, cv2.COLOR_RGBA2RGB)
-#     OUTPUT_PATH = "../results/SegMasks/seg_" + os.path.basename(PATH)
-#     cmd = ["backgroundremover", "-i", PATH, "-m", "u2net", "-o",  OUTPUT_PATH]
-#     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#     output = cv2.imread("output.png", cv2.COLOR_RGBA2RGB)
-#
-#     landmarks = get_landmarks("../dataset/hands/healthy/hand1.jpg", "../hand_landmarker.task").hand_landmarks[0]
-#     roi = get_finger_roi(landmarks, "INDEX")
-#     roi_pixel = get_landmarks_pixel(roi, image.shape[:2])
-#     x, y, w, h = get_bounding_box(roi_pixel)
-#
-#     roi_image = image[y:y + h, x:x + w].astype(np.uint8)
-#
-#     # roi_image = segment_image(roi_image, "../selfie_multiclass_256x256.tflite")
-#
-#     cv2.imwrite("roi.jpg", output)
-#     # Display the ROI
-#     # cv2.namedWindow("ROI Image", cv2.WINDOW_NORMAL)
-#     # cv2.imshow("ROI Image", roi_image)
-#     # cv2.waitKey(0)
-#     # cv2.destroyAllWindows()
 
 def process_image(PATH, OUTPUT_DIR):
     image = cv2.imread(PATH, cv2.COLOR_RGBA2RGB)
     OUTPUT_PATH = os.path.join(OUTPUT_DIR, "seg_" + os.path.basename(PATH))
+    # result = backgroundremover.bg.remove(image)
+    # cv2.imwrite(OUTPUT_PATH, result)
     cmd = ["backgroundremover", "-i", PATH, "-m", "u2net", "-o", OUTPUT_PATH]
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = cv2.imread(OUTPUT_PATH, cv2.COLOR_RGBA2RGB)
     print(os.path.basename(PATH))
-    # Assuming the landmarks are same for all images (which may not be the case, adjust if needed)
-    landmarks = get_landmarks(PATH, "../hand_landmarker.task")
-    if not landmarks.hand_landmarks:
-        return
-    else:
-        landmarks = landmarks.hand_landmarks[0]
-    roi = get_finger_roi(landmarks, "INDEX")
-    roi_pixel = get_landmarks_pixel(roi, image.shape[:2])
-    x, y, w, h = get_bounding_box(roi_pixel)
+    # _,  landmarks = load_image_and_locate_landmarks(OUTPUT_PATH, "hand_landmarker.task")
+    image = cv2.imread(OUTPUT_PATH, cv2.COLOR_RGBA2RGB)
+    
 
-    roi_image = image[y:y + h, x:x + w].astype(np.uint8)
-    # roi_image = segment_image(roi_image, "../selfie_multiclass_256x256.tflite")
-
-    roi_output_path = os.path.join(OUTPUT_DIR, "roi_" + os.path.basename(PATH))
-    # cv2.imwrite(roi_output_path, output)
+        
+    seg_mask = get_segmentation_mask(image)
+    contour = extract_contour(seg_mask)
+    cv2.drawContours(seg_mask, [contour], 0, 128, 2)
+    cv2.namedWindow('Image with Contour', cv2.WINDOW_NORMAL)
+    cv2.imshow('Image with Contour', seg_mask)
+    cv2.waitKey(0)
+    # # if not landmarks.hand_landmarks:
+    # #     return
+    # # else:
+    # #     landmarks = landmarks.hand_landmarks[0]
+    # cv2.imwrite(OUTPUT_PATH, output)
+    
 
 
 if __name__ == "__main__":
-    DIR_PATH = "../dataset/hands/swolen/"
-    OUTPUT_DIR = "../results/SegMasks/"
+    DIR_PATH = "dataset/hands/swolen/"
+    OUTPUT_DIR = "results/SegMasks/"
 
     # Make sure the output directory exists
     if not os.path.exists(OUTPUT_DIR):
@@ -127,3 +109,4 @@ if __name__ == "__main__":
         if image_name.endswith(('.jpg', '.jpeg', '.png')):  # checking file extension
             image_path = os.path.join(DIR_PATH, image_name)
             process_image(image_path, OUTPUT_DIR)
+
